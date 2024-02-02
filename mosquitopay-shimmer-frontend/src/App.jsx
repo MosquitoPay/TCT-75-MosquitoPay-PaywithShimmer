63a56b1e35a6fba78e7a80dc47f63d79be937372
StRane <umutseber4@gmail.com>
2024-02-03 00:42:41 +0700

    short vite update

--- mosquitopay-shimmer-frontend/src/App.jsx
+++ mosquitopay-shimmer-frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import React, {
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -24,34 +20,58 @@ import { Label } from '@/components/ui/label';
 // } from '@/components/ui/select';
 
 export default function App() {
-  const [shop, setShop] = useState({})
-  const backendUrl = "http://localhost:8888/shop"
-  const hasShop = useRef(false)
+  const [shop, setShop] = useState('');
+  const [queryParams, setQueryParams] = useState({});
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const formRef = useRef();
 
   useEffect(() => {
-    console.log({ backendUrl })
-    if (!hasShop.current) {
+    console.log({ backendUrl });
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(searchParams.entries());
+
+    setQueryParams(params);
+
+    if (!shop) {
       fetch(backendUrl)
-      .then((response) => {
-        response.json()
-        .then((resp) => {
-          console.log({ resp })
-          if (resp.shop) {
-            setShop(resp)
-            hasShop.current = true
-          }
+        .then((response) => {
+          response
+            .json()
+            .then((resp) => {
+              console.log({ resp });
+              if (resp.shop) {
+                setShop(resp);
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+            });
         })
-        .catch((err) => {
-          console.error(err)
-        })
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+        .catch((error) => {
+          console.error(error);
+        });
     } else {
-      console.log({ shop })
+      console.log({ shop });
     }
   }, []);
+
+  const submitHandler = async () => {
+    const formData = new FormData(formRef.current);
+    const data = Object.fromEntries(formData.entries());
+
+    let response = await fetch('/api/balance', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+
+      credentials: 'include',
+    });
+
+    console.log(response);
+  };
 
   return (
     <>
@@ -59,12 +79,10 @@ export default function App() {
         <Card className="w-[360px] mx-auto my-auto">
           <CardHeader>
             <CardTitle>Register shop</CardTitle>
-            <CardDescription>
-              Register your shop in one-click.
-            </CardDescription>
+            <CardDescription>Register your shop in one-click.</CardDescription>
           </CardHeader>
-          <CardContent>
-            <form>
+          <form ref={formRef} onSubmit={submitHandler}>
+            <CardContent>
               <div className="grid w-full items-center gap-4">
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="name">Shop URL</Label>
@@ -75,11 +93,11 @@ export default function App() {
                   <Input id="details" type="file" accept="application/json" />
                 </div>
               </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button>Register</Button>
-          </CardFooter>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button type="submit">Register</Button>
+            </CardFooter>
+          </form>
         </Card>
       </div>
     </>
